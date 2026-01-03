90720d3169f3b3e347764e76e1200519a90923b5
csgotom100 <109570423+csgotom100@users.noreply.github.com>
2026-01-03 20:03:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json, urllib.request, yaml, os, ssl, warnings, re, base64
 warnings.filterwarnings("ignore")
 
 # --- 1. 配置源 ---
-# 确保每个链接都单独占一行，且双引号成对出现
+# 使用标准列表格式，确保每个 URL 都是完整的字符串
 FIXED_SOURCES = [
     "https://www.gitlabip.xyz/Alvin9999/PAC/refs/heads/master/backup/img/1/2/ipp/hysteria2/1/config.json",
     "https://www.gitlabip.xyz/Alvin9999/PAC/refs/heads/master/backup/img/1/2/ipp/hysteria2/2/config.json",
@@ -21,4 +21,20 @@ FIXED_SOURCES = [
     "https://fastly.jsdelivr.net/gh/Alvin9999/PAC@latest/backup/img/1/2/ipp/hysteria2/2/config.json",
     "https://gitlab.com/free9999/ipupdate/-/raw/master/backup/img/1/2/ipp/hysteria2/3/config.json",
     "https://fastly.jsdelivr.net/gh/Alvin9999/PAC@latest/backup/img/1/2/ipp/hysteria2/4/config.json",
-    "https://gitlab.com/free9999/ipupdate/-/raw/master/backup/img/1/2/ipp/clash.
+    "https://gitlab.com/free9999/ipupdate/-/raw/master/backup/img/1/2/ipp/clash.meta2/1/config.yaml",
+    "https://fastly.jsdelivr.net/gh/Alvin9999/PAC@latest/backup/img/1/2/ipp/clash.meta2/2/config.yaml",
+    "https://gitlab.com/free9999/ipupdate/-/raw/master/backup/img/1/2/ipp/clash.meta2/3/config.yaml",
+    "https://fastly.jsdelivr.net/gh/Alvin9999/PAC@latest/backup/img/1/2/ipp/clash.meta2/4/config.yaml",
+    "https://gitlab.com/free9999/ipupdate/-/raw/master/backup/img/1/2/ipp/clash.meta2/5/config.yaml",
+    "https://fastly.jsdelivr.net/gh/Alvin9999/PAC@latest/backup/img/1/2/ipp/clash.meta2/6/config.yaml",
+    "https://gitlab.com/free9999/ipupdate/-/raw/master/backup/img/1/2/ipp/singbox/1/config.json",
+    "https://fastly.jsdelivr.net/gh/Alvin9999/PAC@latest/backup/img/1/2/ip/singbox/2/config.json"
+]
+
+MANUAL_FILE = './urls/manual_json.txt'
+OUTPUT_DIR = './sub'
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+ctx = ssl._create_unverified_context()
+
+# --- 2. 解析逻辑 ---
+def get_node_info
